2861dcfbbbd2ef22faf1a589e85982d9ba6fe8c4
Shawn Hassen <Shassen89@gmail.com>
2018-11-16 17:18:47 -0500

    Nav menu is fully functional. I need to continue working on responsive design after completing the rest of the components

--- website-client/src/components/Nav.jsx
+++ website-client/src/components/Nav.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 export default function Nav(props) {
 
+
   return (
-    <div></div>
+    <div className="navmenu">
+      <div id="main">
+        <div className="container" onClick={()=> {
+          document.querySelector("#mySidenav").style.width = "250px";
+          // document.querySelector("#main").style.marginLeft = "250px"; 
+        }}>
+          <div className="hamnav">&#9776;</div>
+        </div>
+      </div>
+
+      <div id="mySidenav" className="sidenav">
+        <div className="closebtn" onClick={()=> {
+          document.querySelector("#mySidenav").style.width = "0";
+          // document.querySelector("#main").style.marginLeft = "0";
+        }}>&times;
+        </div>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Resume</a>
+        <a href="#">Contact</a> 
+      </div>
+    </div>
   )
 
 }
